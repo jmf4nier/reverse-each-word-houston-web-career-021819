40770ea11844f5942c34fdcaf05cdc9eeb3237ca
Jason Fournier <jmf4nier@hotmail.com>
2019-02-01 23:26:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,11 @@ sentence = "Hello there, how are you?"
 
 def reverse_each_word(string)
   array = string.split
-  array.each do |word|
+  #array.each do |word|
     
-    array << word.reverse
+   # array << word.reverse
     
-  end
-  array  
+ # end
+ # array  
   
 end

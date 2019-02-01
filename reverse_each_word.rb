2dc8fe88dd91de70f9c0848b3b21a2f18d68e3d7
Jason Fournier <jmf4nier@hotmail.com>
2019-02-01 23:50:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,11 +5,11 @@ def reverse_each_word(string)
   array = string.split
  
   array.each do |word|
-     puts word.reverse
+    array.push(word.reverse)
     
   end
     
-  
+  puts array
   
   
 end

0adf49f07a0fc25a71cf04ee21bf26d430a6b3ea
Jason Fournier <jmf4nier@hotmail.com>
2019-02-01 23:49:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,14 +3,14 @@ sentence = "Hello there, how are you?"
 
 def reverse_each_word(string)
   array = string.split
-  puts array
+ 
   array.each do |word|
      puts word.reverse
     
   end
     
   
-  puts array
+  
   
 end
 reverse_each_word(sentence)
